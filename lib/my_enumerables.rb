f8735124d5b418b07a7cffa96fdeecf5861862c5
Slavetomints <161798204+Slavetomints@users.noreply.github.com>
2024-07-28 22:56:42 -0500

    Update my_enumerables.rb

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,95 @@
 module Enumerable
-  # Your code goes here
+  def my_all?(&block)
+    for item in self
+      if block.call(item) == false
+        return false
+      end
+    end
+    return true
+  end
+
+  def my_any?(&block)
+    for item in self
+      if block.call(item) == true
+        return true
+      end
+    end
+    return false
+  end
+
+  def my_count(&block)
+    count = 0
+    for item in self
+      if block_given?
+        if block.call(item) == true
+        count += 1
+        end
+      else count += 1
+      end
+    end
+    count
+  end
+
+  def my_each_with_index(&block)
+    if block_given?
+      index = 0
+      self.my_each do |var|
+        block.call(var, index)
+        index += 1
+      end
+    end
+  end
+
+  def my_inject(start, &block)
+    if block_given?
+      initial_value = 0
+      current_index = 0
+      for item in self
+        if initial_value == 0
+          sum = block.call(start, self[current_index])
+          current_index += 1
+          initial_value += 1
+        else
+          sum = block.call(sum, self[current_index])
+          current_index += 1
+        end
+      end
+      sum
+    end
+  end
+
+  def my_map(&block)
+    if block_given?
+      arr = []
+      for item in self
+        arr << block.call(item)
+      end
+    end
+    arr
+  end
+
+  def my_none?(&block)
+    if block_given?
+      for item in self
+        if block.call(item) == true
+          return false
+        end
+      end
+    end
+    true
+  end
+
+  def my_select(&block)
+    if block_given?
+      arr = []
+      for item in self
+        if block.call(item) == true
+          arr << item
+        end
+      end
+    end
+    arr
+  end
 end
 
 # You will first have to define my_each
@@ -7,5 +97,9 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  def my_each
+    for item in self
+      yield item
+    end
+  end
 end
